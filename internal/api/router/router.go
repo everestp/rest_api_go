c2daefed71fcd7c5a7d913c0d7195ebc922b5b37
everestp <paudeleverest09@gmail.com>
2026-01-16 04:28:42 +0545

    220. Execs Router

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -2,16 +2,13 @@ package router
 
 import (
 	"net/http"
-
-	"github.com/everestp/rest_api_go/internal/api/handlers"
 )
 
-// Router initializes all routes and returns a ServeMux.
 func MainRouter() *http.ServeMux {
+
 	tRouter := teacherRouter()
 	sRouter := studentRouter()
-
+	sRouter.Handle("/", execsRouter())
 	tRouter.Handle("/", sRouter)
-	return  tRouter
-
+	return tRouter
 }
